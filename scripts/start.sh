e9b3d2f31558ce3ec0d8fe3a59bc81ab9d6ee240
brendanshort <brendan.short@digital.cabinet-office.gov.uk>
2014-10-27 13:04:07 +0000

    Updated start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ./node_modules/grunt-cli/bin/grunt buildcss
 
-python ./scripts/server.py localhost & wait
-./scripts/start-server.sh && fg
+python ./scripts/server.py localhost & ./scripts/start-server.sh && fg
